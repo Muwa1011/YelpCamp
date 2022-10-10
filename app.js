6062ae62625980fe1519ead4ab3fa887bc1330d8
Muwa1011 <46922312+Muwa1011@users.noreply.github.com>
2022-10-10 17:30:21 +0200

    Delete

--- app.js
+++ app.js
@@ -64,6 +64,12 @@ app.put('/campgrounds/:id', async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
+app.delete('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params
+    await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+})
+
 app.get('/makecampground', async (req, res) => {
     const camp = new Campground({title: 'My Backyard', description: 'cheap camping'})
     await camp.save()
