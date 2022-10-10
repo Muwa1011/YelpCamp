263abf07fc8468000a03848a4f5f2f27a1042929
Muwa1011 <46922312+Muwa1011@users.noreply.github.com>
2022-10-10 17:12:26 +0200

    Create

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const exp = require("constants");
 const express = require("express")
 const mongoose = require('mongoose')
 const path = require('path');
+const campground = require("./models/campground");
 const Campground = require('./models/campground')
 
 const app = express();
@@ -20,6 +22,8 @@ db.once("open", () => {
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true}))
+
 app.listen(3000, () => {
     console.log("listening on port 3000")
 })
@@ -33,10 +37,21 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground })
 })
+
 
 app.get('/makecampground', async (req, res) => {
     const camp = new Campground({title: 'My Backyard', description: 'cheap camping'})
