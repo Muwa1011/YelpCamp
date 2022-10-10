4edc5648f80c0450a6e0e59e5dd03d878af9c583
Muwa1011 <46922312+Muwa1011@users.noreply.github.com>
2022-10-10 16:21:06 +0200

    seeds

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const mongoose = require('mongoose')
 const path = require('path');
-const campground = require("./models/campground");
 const Campground = require('./models/campground')
 
 const app = express();
