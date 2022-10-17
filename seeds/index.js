ca34a1984e83fdc22cf97761b7a93cb6afbc4bdd
Muwa1011 <46922312+Muwa1011@users.noreply.github.com>
2022-10-17 14:41:54 +0200

    camp images

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Campground = require('../models/campground')
-const {places, descriptors} = require('./seedHelpers')
+const { places, descriptors } = require('./seedHelpers')
 const cities = require('./cities')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -18,11 +18,15 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 
 const seedDB = async () => {
     await Campground.deleteMany({})
-    for(let i = 0; i < 50; i++){
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores reiciendis ipsa laudantium minus ipsam accusamus quae provident voluptas. Perferendis nulla dolorum assumenda fugiat repudiandae ipsa ut dolorem accusamus commodi amet!",
+            price: price
         })
         await camp.save()
     }
